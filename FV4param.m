b9b1a00ba51f07318c5bb008393f03d83a1801a8
LeoJW <leo@woodtx.com>
2020-05-02 14:23:44 -0500

    Force-velocity function lifted from previous modeling

---
+++ FV4param.m
@@ -0,0 +1,21 @@
+function [F] = FV4param(c,v)
+
+c1 = c(1); %Curve shape during contracting phase
+c2 = c(2); %Curvature
+c3 = (c1.^2 + c1)/c1.^2; %Slope at zero 
+cmax = c(3); %Asymptote as v approaches -inf
+vmax = c(4); %Max velocity; assumes velocity input is actual length/s
+kl = (c2*(1-cmax))/(c3*(1+c2));
+
+v = v./vmax;
+F = zeros(size(v));
+
+for i = 1:length(v)
+    if v(i) <= 0
+        F(i) = (1 + v(i))/(1 - v(i)/c1);
+    else
+        F(i) = (kl - cmax*v(i))/(kl-v(i));
+    end
+end
+end
+

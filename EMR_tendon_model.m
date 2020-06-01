6a989a0991a045bdaff0dd73db242f182ecef32a
Francesca Ciocca <francescaciocca@Francescas-MacBook-Pro.local>
2020-06-01 11:07:54 -0700

    choosing initial conditions to match first Ft w/ first hill force

--- EMR_tendon_model.m
+++ EMR_tendon_model.m
@@ -143,21 +143,20 @@ lmt = A2.*sin(w.*t3) + 2; % MTU length
 vmt = A2.*w.*cos(w*t3); % MTU velocity
 
 % Initial conditions
-x0 = [0.8,0]; % muscle [xm,vm]
+x0 = [1.1461,0]; % muscle [xm,vm]
 
 % Preallocate
 k = 0.1; % spring constant
 xm = [x0(1), zeros(1,n-1)]; % muscle length
 vm = [x0(2), zeros(1,n-1)]; % muscle velocity
 Ft = [k.*(lmt(1)-x0(1)), zeros(1,n-1)]; % tendon force
-mindiff = [abs(k.*(lmt(1)-x0(1)) - hill(x0(1),x0(2),a(1),C)), zeros(1,n-1)];
 
 % mindiff = k(l-x) - hill(x,v,a,C); want value of vrange that minimizes mindiff
 
 for i = 2:n
     xm(i) = xm(i-1) + vm(i-1)*dt; % problem here with vm
     Ft(i) = k.*(lmt(i)-xm(i));
-    mindiff = abs(k.*(lmt(i)-xm(i)) - hill(xm(i),vrange,a(i),C));
+    mindiff = abs(Ft(i) - hill(xm(i),vrange,a(i),C));
     [~,index] = min(mindiff); % should correspond to index of vrange that minimizes
     vm(i) = vrange(index); % value in vrange that minimizes mindiff
 end

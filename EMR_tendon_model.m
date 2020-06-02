a6c33c27ab81330d7fc791ac63676ee70dc6ec5d
Francesca Ciocca <francescaciocca@Francescas-MacBook-Pro.local>
2020-06-02 09:18:55 -0700

    tried crappy method for defining u, doesn't work well

--- EMR_tendon_model.m
+++ EMR_tendon_model.m
@@ -2,6 +2,7 @@
 clear vars;
 
 % FL curves are normalized force plotted against L/Lopt
+% velocity in (L/Lopt)/sec
 % FV curve is normalized force plotted against v/vmax
 
 %% Hill Model
@@ -9,14 +10,20 @@ clear vars;
 % FL active component function
 FLactFunc = @(b,x) exp(-(((x-b(2))-1)./b(1)).^2);
 
+%% Finding vm with brute force
+
 % Variables
-hz = 500; % sample rate in samples/s
-nsamp = linspace(0,1000,10e3); % number of samples
-t = nsamp./hz; % time in seconds
-w = 8.6; % cycle frequency in rad/s
-A = 0.1; % amplitude of x
-x = A.*sin(w.*t) + 0.8; % L/Lopt
-v = A.*w.*cos(w.*t); % Lengths/sec
+
+time = 9.999; % seconds
+dt = 0.001; % time step
+t2 = 0:dt:time; % time vector, 1e4 long
+w = 8; % frequency in Hz or cycles/s
+niter = length(t2); % number of iterations in loop
+lcycle = niter./w; % cycle length in 1/1e4 s
+duration = 0.5; % duration of cycle that is activated (scaled 0 to 1)
+tstart = 0.1;% point in cycle where activation begins (scaled 0 to 1)
+stt2 = tstart.*lcycle;
+endact = (tstart+duration).*lcycle;
 
 b1 = 0.25; % FLact
 b2 = 0; % FLact
@@ -32,129 +39,47 @@ c1 = 0.29; % from Biewener et al. (2014)
 c2 = 1; % overall curvature of FV
 fvc = [c1,c2,cmax,vmax];
 
-d = 50; % activation delay, in ms
-gam1 = -0.993; % activation
-gam2 = -0.993; % activation
-
-% Lu et al. (2011)
-FV_lu = FV4param(fvc,v); % Force-velocity
-figure(1)
-plot(v,FV_lu), xlabel("Normalized Velocity"), ylabel("Normalized Force")
-
-FLpas_lu = FLpasFunc(p,x); % FL passive
-figure(2)
-plot(x,FLpas_lu), xlabel("Normalized Length"), ylabel("Normalized Force")
-
-FLact_lu = FLactFunc(b,x); % FL active
-figure(3)
-plot(x,FLact_lu), xlabel("Normalized Length"), ylabel("Normalized Force")
-
-%% Vectors for dummy work loops
+d = 50; % activation delay, in ms -> might need to rescale
+gam1 = -0.993; % activation constant
+gam2 = -0.993; % activation constant
 
 % Neural excitation, vector of zeros except one chunk which is 1s
-u = zeros(1,10e3);
-u(300:800) = 1;
+u = zeros(1,1e4);
+u(stt2:endact) = 1;
+u((lcycle+stt2):(lcycle+endact)) = 1;
+u((2.*lcycle+stt2):(2.*lcycle+endact)) = 1;
+u((3.*lcycle+stt2):(3.*lcycle+endact)) = 1;
+u((4.*lcycle+stt2):(4.*lcycle+endact)) = 1;
+u((5.*lcycle+stt2):(5.*lcycle+endact)) = 1;
+u((6.*lcycle+stt2):(6.*lcycle+endact)) = 1;
+u((7.*lcycle+stt2):(7.*lcycle+endact)) = 1;
 
-%% Activation function
+% Activation function
 a = activationODE2(u,d,gam1,gam2);
 
-%% Hill function
-
-% Vector for all Hill constants
+% Vector for Hill constants
 C = [b1,b2,p1,p2,c1,c2,cmax,vmax,Fmax];
-
-figure(4)
-hilltest = hill(x,v,a,C);
-plot(x,hilltest), xlabel("L/Lopt"), ylabel("Force")
-
-%% Tendon Stuff
-
-% Length of tendon could be expressed as tlength/Lopt??
-
-%Constants
-% k = 0.1; % spring constant
-% M = 5; % mass
-% simTime = 10; %seconds
-% dt = 0.01; % time step should be small: the smaller, the more accurate
-% t2 = 0:dt:simTime; % time divided into time steps
-% niter = length(t2);
-
-%Initial Conditions
-% x0 = [2,0]; % muscle [position,velocity]
-% l0 = [3,0]; % MTU [position,velocity]
-% 
-% %Preallocate for loop
-% xm = [x0(1), zeros(1,niter-1)]; % muscle length
-% vm = [x0(2), zeros(1,niter-1)]; % muscle velocity
-% 
-% lmt = [l0(1), zeros(1,niter-1)]; % MTU length
-% vmt = [l0(2), zeros(1,niter-1)]; % MTU velocity -- we should know this as an input
-
-% either need to solve for acceleration or pull velocity from hill model,
-% or solve for velocity in some other way
-
-% Need vm to solve for F(i) needed in minimization.
-% HOW to get Ft(i) for minimization w/o solving for it first?
-
-% Ft = [k.*(l0(1)-x0(1)), zeros(1,niter-1)]; % tendon force, equal to Fm
-% dFt = [k.*(l0(2)-x0(2)), zeros(1,niter-1)]; % derivative of Ft or Fm
-
-% dFt(i) = (k.*(lmt(i+1)-xm(i+1)-lmt(i)+xm(i)))./dt;
-% Ft(i) = Ft(i-1) + dFt(i-1)*dt;
-% vm(i) = vm(i-1) + am(i-1)*dt;
-% vmt(i) = vmt(i-1) + amt(i-1)*dt;
-
-%% Finding vm with minimization
-
-% Bounds
-% lb = 0;
-% ub = 20;
-% 
-% % Initial conditions
-% x0 = [2,1]; % muscle [position,velocity]
-% l0 = [3,1]; % MTU [position,velocity]
-% v0 = 1;
-
-% % Preallocate
-% k = 5; % spring constant
-% xm = [x0(1), zeros(1,niter-1)]; % muscle length
-% vm = [x0(2), zeros(1,niter-1)]; % muscle velocity
-% lmt = [l0(1), zeros(1,niter-1)]; % MTU length
-% vmt = [l0(2), zeros(1,niter-1)]; % MTU velocity
-% Ft = [k(lmt-xm), zeros(1,niter-1)]; % tendon force, equal to Fm
-% dFt = [k(vmt-vm), zeros(1,niter-1)]; % derivative of Ft or Fm
-
-% Fvmin = Ft-hill
-% Ft = kx
-
-% Minimization
-
-%% Finding vm with brute force
 
 k = 0.1; % spring constant
 vrange = linspace(-20,20,1e4); % range of possible muscle velocities
-time = 9.999; % seconds
-dt = 0.001; % time step
-t3 = 0:dt:time;
-n = length(t3);
 
-A2 = 0.2; % amplitude of x
-lmt = A2.*sin(w.*t3) + 2; % MTU length
-vmt = A2.*w.*cos(w*t3); % MTU velocity
+A2 = 0.2; % amplitude of lmt
+lmt = A2.*sin(w.*t2) + 2; % MTU length, lmt/Lopt
+vmt = A2.*w.*cos(w*t2); % MTU velocity, vmt/Lopt
 
 % Initial conditions
 x0 = [1.1461,0]; % muscle [xm,vm]
 
 % Preallocate
-k = 0.1; % spring constant
-xm = [x0(1), zeros(1,n-1)]; % muscle length
-vm = [x0(2), zeros(1,n-1)]; % muscle velocity
-Ft = [k.*(lmt(1)-x0(1)), zeros(1,n-1)]; % tendon force
+xm = [x0(1), zeros(1,niter-1)]; % muscle length
+vm = [x0(2), zeros(1,niter-1)]; % muscle velocity
+Ft = [k.*(lmt(1)-x0(1)), zeros(1,niter-1)]; % tendon force
+dFt = [k.*(vmt(1)-x0(2)), zeros(1,niter-1)]; % deriv of tendon force
 
 % mindiff = k(l-x) - hill(x,v,a,C); want value of vrange that minimizes mindiff
 
-for i = 2:n
-    xm(i) = xm(i-1) + vm(i-1)*dt; % problem here with vm
+for i = 2:niter
+    xm(i) = xm(i-1) + vm(i-1).*dt; % problem here with vm
     Ft(i) = k.*(lmt(i)-xm(i));
     mindiff = abs(Ft(i) - hill(xm(i),vrange,a(i),C));
     [~,index] = min(mindiff); % should correspond to index of vrange that minimizes

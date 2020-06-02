2e870f70f9ffe62f742274485d6a20d49fdcc8e7
Francesca Ciocca <francescaciocca@Francescas-MacBook-Pro.local>
2020-06-02 14:21:41 -0700

    redefining u

--- EMR_tendon_model.m
+++ EMR_tendon_model.m
@@ -14,16 +14,18 @@ FLactFunc = @(b,x) exp(-(((x-b(2))-1)./b(1)).^2);
 
 % Variables
 
-time = 9.999; % seconds
-dt = 0.001; % time step
-t2 = 0:dt:time; % time vector, 1e4 long
-w = 8; % frequency in Hz or cycles/s
-niter = length(t2); % number of iterations in loop
-lcycle = niter./w; % cycle length in 1/1e4 s
-duration = 0.5; % duration of cycle that is activated (scaled 0 to 1)
+w = 4; % frequency in Hz or cycles/s
+ncycles = 16; % number of cycles
 tstart = 0.1;% point in cycle where activation begins (scaled 0 to 1)
-stt2 = tstart.*lcycle;
-endact = (tstart+duration).*lcycle;
+duration = 0.5; % duration of cycle that is activated (scaled 0 to 1)
+
+totaltime = ncycles/w; % time in s
+t = linspace(0,totaltime,1e4); % time vector, 1e4 long
+dt = totaltime/length(t); % time step
+niter = length(t); % number of iterations in loop
+lcycle = niter/ncycles; % cycle length in 1/1e4 s
+startdur = tstart*lcycle; % start of activation in cycle
+enddur = startdur + duration*lcycle; % duration of cycle activated in 1/1e4 s
 
 b1 = 0.25; % FLact
 b2 = 0; % FLact
@@ -44,15 +46,9 @@ gam1 = -0.993; % activation constant
 gam2 = -0.993; % activation constant
 
 % Neural excitation, vector of zeros except one chunk which is 1s
-u = zeros(1,1e4);
-u(stt2:endact) = 1;
-u((lcycle+stt2):(lcycle+endact)) = 1;
-u((2.*lcycle+stt2):(2.*lcycle+endact)) = 1;
-u((3.*lcycle+stt2):(3.*lcycle+endact)) = 1;
-u((4.*lcycle+stt2):(4.*lcycle+endact)) = 1;
-u((5.*lcycle+stt2):(5.*lcycle+endact)) = 1;
-u((6.*lcycle+stt2):(6.*lcycle+endact)) = 1;
-u((7.*lcycle+stt2):(7.*lcycle+endact)) = 1;
+ucycle = zeros(1,lcycle);
+ucycle(startdur:enddur) = 1;
+u = repmat(ucycle,ncycles);
 
 % Activation function
 a = activationODE2(u,d,gam1,gam2);
@@ -64,8 +60,8 @@ k = 0.1; % spring constant
 vrange = linspace(-20,20,1e4); % range of possible muscle velocities
 
 A2 = 0.2; % amplitude of lmt
-lmt = A2.*sin(w.*t2) + 2; % MTU length, lmt/Lopt
-vmt = A2.*w.*cos(w*t2); % MTU velocity, vmt/Lopt
+lmt = A2.*sin(w.*t) + 2; % MTU length, lmt/Lopt
+vmt = A2.*w.*cos(w*t); % MTU velocity, vmt/Lopt
 
 % Initial conditions
 x0 = [1.1461,0]; % muscle [xm,vm]

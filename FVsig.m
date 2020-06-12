8602bf1d1556304b31536342a02cb3955d00165a
Francesca Ciocca <francescaciocca@Francescas-MacBook-Pro.local>
2020-06-12 10:30:45 -0700

    sigmoid version of FV curve

---
+++ FVsig.m
@@ -0,0 +1,11 @@
+function [FV] = FVsig(s,v)
+% Sigmoid version of FV function
+
+s1 = s(1);
+s2 = s(2); % 1/cmax, "asymptote", upper limit
+s3 = s(3);
+s4 = s(4); % affects steepness of slope at 0
+FV = s1/(s2 + s3.*exp(s4*v));
+
+end
+

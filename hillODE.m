8a9ab36175b49c3c5aa9dd8678282f2d9b4cd801
Francesca Ciocca <francescaciocca@Francescas-MacBook-Pro.local>
2020-06-11 21:00:02 -0700

    added tvec, made x and F into single vector

--- hillODE.m
+++ hillODE.m
@@ -1,4 +1,4 @@
-function [xdot,Fdot] = hillODE(t,l,ldot,x,F,a)
+function [xdot,Fdot] = hillODE(t,tvec,l,ldot,x,F,a)
 % ODE for MTU
 
 % FL active component
@@ -15,14 +15,15 @@ FLpas = FLpasFunc(p,x);
 % FV sigmoid function for ref: FVsig = s1/(s2 + s3.*exp(s4*v));
 % rearranged below to solve for xdot (velocity)
 
-xt = linspace(1,10,1e4);
-Ft = linspace(1,10,1e4);
+tvec = linspace(1,10,1e4)
 
 x = interp1(xt,x,t);
-F = interp1(Ft,F,t);
+F = interp1(Ft,F,t)];
+state = [x,F];
 
 xdot = (ln(((s1*Fmax.*FLact.*a)/(k(l-x)-FLpas) - s2)./s3))./s4; % dx/dt
 Fdot = k(ldot - (ln(((s1*Fmax.*exp(-((((l-(F/k))-b2)-1)./b1).^2).*a)/(F-p1.*((l-(F/k))-p2).^2) - s2)./s3))./s4); % dF/dt
+dsdt = [xdot,Fdot];
 
 end
 

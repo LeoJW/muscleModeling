5eeb9cd2faed7ec3f461aae665f4b44ab70f4e66
Francesca Ciocca <francescaciocca@Francescas-MacBook-Pro.local>
2020-06-11 20:31:07 -0700

    added Ft and xt for interpolation

--- hillODE.m
+++ hillODE.m
@@ -1,4 +1,4 @@
-function [xdot,Fdot] = hillODE(t,x,l,xdot,ldot,F,a)
+function [xdot,Fdot] = hillODE(t,l,ldot,x,F,a)
 % ODE for MTU
 
 % FL active component
@@ -10,13 +10,16 @@ FLact = FLactFunc(b,x);
     y = p(1).*(x-p(2)).^2;
     y(x<p(2)) = 0;
     end
-FLpas = FLpasFunc(p,x)
+FLpas = FLpasFunc(p,x);
 
 % FV sigmoid function for ref: FVsig = s1/(s2 + s3.*exp(s4*v));
 % rearranged below to solve for xdot (velocity)
 
-x = interp1(xt,x,t); % ???
-F = interp1(Ft,F,t); % ???
+xt = linspace(1,10,1e4);
+Ft = linspace(1,10,1e4);
+
+x = interp1(xt,x,t);
+F = interp1(Ft,F,t);
 
 xdot = (ln(((s1*Fmax.*FLact.*a)/(k(l-x)-FLpas) - s2)./s3))./s4; % dx/dt
 Fdot = k(ldot - (ln(((s1*Fmax.*exp(-((((l-(F/k))-b2)-1)./b1).^2).*a)/(F-p1.*((l-(F/k))-p2).^2) - s2)./s3))./s4); % dF/dt

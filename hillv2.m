e00c6aee4a594e9b278e7012f6e77a8b223fc0a8
Francesca Ciocca <francescaciocca@Francescas-MacBook-Pro.local>
2020-06-14 13:03:17 -0700

    changed C to C2

--- hillv2.m
+++ hillv2.m
@@ -1,20 +1,20 @@
-function [H2] = hillv2(x,v,a,C)
+function [H2] = hillv2(x,v,a,C2)
 % Hill function version 2 w/ sigmoid FV curve
 
 
 % Variables in C, descriptions listed in functions
-b1 = C(1);
-b2 = C(2);
-p1 = C(3);
-p2 = C(4);
-s1 = C(5);
-s2 = C(6);
-s3 = C(7);
-s4 = C(8);
-Fmax = C(9);
+b1 = C2(1);
+b2 = C2(2);
+p1 = C2(3);
+p2 = C2(4);
+s1 = C2(5);
+s2 = C2(6);
+s3 = C2(7);
+s4 = C2(8);
+Fmax = C2(9);
 
-b = C(1:2);
-p = C(3:4);
+b = C2(1:2);
+p = C2(3:4);
 fvs = C(5:8);
 
 FLactFunc = @(b,x) exp(-(((x-b(2))-1)./b(1)).^2); % FL active component

3b1153cf07e2fa942163d07761896e4d951dd6d1
Francesca Ciocca <francescaciocca@Francescas-MacBook-Pro.local>
2020-06-14 12:45:35 -0700

    defined vmax and reshuffled constants slightly for coherence

--- FVsig.m
+++ FVsig.m
@@ -2,12 +2,14 @@ function [FV] = FVsig(s,v)
 % Sigmoid version of FV function
 
 s1 = s(1);
-s2 = s(2); % 1/cmax, "asymptote", upper limit
-s3 = s(3);
-s4 = s(4); % affects steepness of slope at 0
+s2 = s(2); 
+s3 = s(3); % affects steepness of slope at 0
+cmax = s(4); % "asymptote", upper limit
+s4 = 1/cmax;
+vmax = s(5);
 
 v = v./vmax;
-FV = s1./(s2 + s3.*exp(-s4*v));
+FV = s1./(s4 + s2.*exp(-s3*v));
 
 end
 

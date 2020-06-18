68e249ddd286aab6c88cd82debbcdbad0b166dd6
Francesca Ciocca <francescaciocca@Francescas-MacBook-Pro.local>
2020-06-18 15:56:11 -0700

    fixed heaviside function definition

--- hillODE.m
+++ hillODE.m
@@ -40,13 +40,16 @@ tl = interp1(tvec,l,t);
 tldot = interp1(tvec,ldot,t);
 ta = interp1(tvec,a,t);
 
+H = heaviside(x-p2);
+
 dxdt = zeros(2,1); % [xdot, Fdot]
 if a > 0
     dxdt(1) = (-1/s3).*(log(((s1/s2)*(Fmax.*FLact.*ta)/(k*(tl-x)-FLpas) - s4/s2))); % dx/dt
     dxdt(2) = k*(tldot - ((-1/s3).*log(((s1/s2)*(Fmax.*exp(-((((tl-F/k)-b2)-1)./b1).^2).*ta)/(F-p1.*((tl-F/k)-p2).^2) - s4/s2)))); % dF/dt
 else
-    dxdt(1) = tldot./((H(x-p2).*(2p1.*(x-p2).^2))/k + 1));
-    dxdt(2) = k*(tldot - tldot./((H((tl-F/k)-p2).*(2*p1*((tl-F/k)-p2).^2))/k + 1));
+    dxdt(1) = tldot./((H.*(2*p1.*(x-p2).^2))/k + 1);
+    dxdt(2) = k*(tldot - tldot./((H.*(2*p1*((tl-F/k)-p2).^2))/k + 1));
+end
 
 end
 

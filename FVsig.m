25adae37b10d399ae9a0d97858d003dd54f0e4f2
Francesca Ciocca <francescaciocca@Francescas-MacBook-Pro.local>
2020-06-23 12:58:37 -0700

    changed comment for cmax

--- FVsig.m
+++ FVsig.m
@@ -1,10 +1,10 @@
 function [FV] = FVsig(s,v)
 % Sigmoid version of FV function
 
-s1 = s(1);
+s1 = s(1); % "asymptote", upper limit
 s2 = s(2); 
 s3 = s(3); % affects steepness of slope at 0
-s4 = s(4); % "asymptote", upper limit
+s4 = s(4);
 vmax = s(5);
 
 v = v./vmax;

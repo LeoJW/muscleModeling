047cefc2beaa80bc1b611e95868b5b48195ce51f
Francesca Ciocca <francescaciocca@Francescas-MacBook-Pro.local>
2020-06-27 10:46:56 -0700

    added linear segment to end

--- FV4param.m
+++ FV4param.m
@@ -13,6 +13,8 @@ F = zeros(size(v));
 for i = 1:length(v)
     if v(i) <= 0
         F(i) = (1 + v(i))/(1 - v(i)/c1);
+    elseif v(i) >= 0.6
+        F(i) = 0.15*v(i) + 1.604;
     else
         F(i) = (kl - cmax*v(i))/(kl-v(i)); 
     end

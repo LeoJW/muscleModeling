adb20410222e7638272aca00aecc8d0b616be538
Francesca Ciocca <francescaciocca@Francescas-MacBook-Pro.local>
2020-07-24 13:25:42 -0700

    added mean function for calculating avg cycle

--- buttersplit.m
+++ buttersplit.m
@@ -1,19 +1,21 @@
-function [outputArg1,outputArg2] = buttersplit(tdata,rawdata,tnew)
-% Function for applying LPF to data and splitting waveforms
-% Trim data
+function meanClean = buttersplit(tdata,rawdata,tnew)
+% Function for cleaning up data and splitting + overlaying waveforms
+%--Trim data
 convertNaN = isnan(rawdata);
-dataTrimmed = rawdata(find(convertNaN>0,1):find(convertNaN,'last'));
-% Interpolate gaps in data
+dataTrimmed = rawdata(find(convertNaN>0,1):find(convertNaN>0,1,'last'));
+%--Interpolate gaps in data
 dataFull = spline(tdata,dataTrimmed,tnew);
-% Apply LPF
+%--Apply LPF
 [beep,boop] = butter(5,0.5);
 dataFilt = filtfilt(beep,boop,dataFull);
-% Find local minima w/ findpeaks
-[mins,locs] = findpeaks(-dataFilt);
-% Use locations of minima to define beginning/end of each cycle
+%--Find local minima w/ findpeaks
+[~,locs] = findpeaks(-dataFilt);
+%--Use locations of minima to define beginning/end of each cycle
 wave = zeros(1,length(locs));
 for i = 1:length(locs)
-    wave(i) = thetaraw(locs(i):locs(i+1));
+    wave(i) = rawdata(locs(i):locs(i+1));
 end
+%--Take avg of all split cycles
+meanClean = mean(wave);
 end
 

50f5f19f09f5adce3031a4e4b3e25423707b40c2
Francesca Ciocca <francescaciocca@Francescas-MacBook-Pro.local>
2020-07-26 11:44:04 -0700

    fixed trimming with NaN and adjusted butterworth filter values

--- buttersplit.m
+++ buttersplit.m
@@ -4,33 +4,36 @@ function meanClean = buttersplit(tdata,rawdata,tnew)
 % tnew is desired new time vector
 %--Trim data
 convertNaN = isnan(rawdata);
-dataTrimmed = rawdata(find(convertNaN>0,1):find(convertNaN>0,1,'last'));
+dataTrimmed = rawdata(find(convertNaN<1,1):find(convertNaN<1,1,'last'));
+timeTrimmed = tdata(find(convertNaN<1,1):find(convertNaN<1,1,'last'));
 %--Interpolate gaps in data
-dataFilled = spline(tdata,dataTrimmed,tnew);
+dataFilled = spline(timeTrimmed,dataTrimmed,tnew);
 %--Apply LPF
-[beep,boop] = butter(5,0.5);
+[beep,boop] = butter(5,0.3);
 dataFilt = filtfilt(beep,boop,dataFilled);
 %--Find local minima w/ findpeaks
 [~,locs] = findpeaks(-dataFilt);
 %--Use locations of minima to define beginning/end of each cycle
 nwaves = length(locs)-1;
 wavedur = zeros(1,nwaves);
+twave = zeros(1,nwaves);
 for i = 1:nwaves+1
     % Define each wave/cycle duration
     wavedur(i) = locs(i+1)-locs(i);
-    % Make new time vector from 0 to 1 for all waves (dimensionless)
-    twave = (locs(i):locs(i+1))./wavedur(i);
+    % Make new time vector so all waves go from 0 to 1 (dimensionless)
+    twave(i) = linspace(0,1,wavedur(i));
     % Prep wave var
-    wave = cell(length(twave),nwaves);
+    wave = cell(length(???),nwaves);
     % Loop through different cycles
     for j = 1:nwaves
         % Declare vars
-        wave{j} = zeros(1,twave);
+        wave{j} = zeros(1,twave(i));
         % Define each wave
         wave{j} = dataFilt(locs(i):locs(i+1));
     end
 end
 %--Take avg/overlay of all cycles
 meanClean = mean(wave); % use another method
+% Overlay, then use Lowess model or equiv
 end
 

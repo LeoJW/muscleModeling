73a804587b84a7e0f83ace0fe04272b19c585c12
Francesca Ciocca <francescaciocca@dhcp-128-189-24-145.ubcsecure.wireless.ubc.ca>
2020-08-03 09:59:30 -0700

    cleaned up wave cycle splitting

--- buttersplit.m
+++ buttersplit.m
@@ -16,26 +16,13 @@ dataFilt = filtfilt(beep,boop,dataFilled);
 %--Use locations of minima to define beginning/end of each cycle
 nwaves = length(locs)-1;
 wavedur = zeros(1,nwaves);
-twave = zeros(1,nwaves);
-for i = 1:nwaves+1
-    % Define each wave/cycle duration
-    wavedur(i) = locs(i+1)-locs(i);
+%twave = zeros(1,nwaves);
+tVecNew = zeros(1,length(timeTrimmed));
+for i = 1:nwaves
     % Make new time vector so all waves go from 0 to 1 (dimensionless)
-    twave(i) = linspace(0,1,wavedur(i));
-    % Prep wave var
-    wave = cell(1,nwaves);
-    % Loop through different cycles
-    for j = 1:nwaves
-        % Declare vars
-        wave{j} = zeros(1,twave(i));
-        % Define each wave
-        wave{j} = dataFilt(locs(i):locs(i+1));
-    end
+    tVecNew(locs(i):locs(i+1)) = linspace(0,1,locs(i+1)-locs(1));
 end
-% Big time vector for overlaying the cycles
-allwaves = wave(1:end);
-t = twave(1:end);
 % Smooth overlaid cycles
-flap = fit([t allwaves],'lowess');
+flap = fit([tVecNew dataFilt],'lowess');
 end
 

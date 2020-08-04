0347c802c4a8da73a7c212400c228d883d2abf42
Francesca Ciocca <francescaciocca@Francescas-MacBook-Pro.local>
2020-08-04 10:19:01 -0700

    lowess filter

--- buttersplit.m
+++ buttersplit.m
@@ -1,4 +1,4 @@
-function [t,flap] = buttersplit(tdata,rawdata,tnew)
+function [flap] = buttersplit(tdata,rawdata,tnew)
 % Function for cleaning up data and splitting + overlaying waveforms
 % tdata is time vector from raw data
 % tnew is desired new time vector
@@ -13,14 +13,12 @@ dataFilled = spline(timeTrimmed,dataTrimmed,tnew);
 dataFilt = filtfilt(beep,boop,dataFilled);
 %--Find local minima w/ findpeaks
 [~,locs] = findpeaks(-dataFilt);
-%--Use locations of minima to define beginning/end of each cycle
+%---Preallocate for loop
 nwaves = length(locs)-1;
-wavedur = zeros(1,nwaves);
-%twave = zeros(1,nwaves);
 tVecNew = zeros(1,length(timeTrimmed));
 for i = 1:nwaves
     % Make new time vector so all waves go from 0 to 1 (dimensionless)
-    tVecNew(locs(i):locs(i+1)) = linspace(0,1,locs(i+1)-locs(1));
+    tVecNew(locs(i):locs(i+1)) = linspace(0,1,locs(i+1)-locs(i)+1);
 end
 % Smooth overlaid cycles
 flap = fit([tVecNew dataFilt],'lowess');

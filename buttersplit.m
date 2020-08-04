75e2f5b85dd53c02c85c23dce9fc0dacb1987c2c
Francesca Ciocca <francescaciocca@Francescas-MacBook-Pro.local>
2020-08-04 15:04:16 -0700

    trimmed NaNs out to overlay cycles, used smoothingspline to smooth

--- buttersplit.m
+++ buttersplit.m
@@ -1,4 +1,4 @@
-function [flap] = buttersplit(tdata,rawdata,tnew)
+function [flap] = buttersplit(tdata,rawdata)
 % Function for cleaning up data and splitting + overlaying waveforms
 % tdata is time vector from raw data
 % tnew is desired new time vector
@@ -7,7 +7,7 @@ convertNaN = isnan(rawdata);
 dataTrimmed = rawdata(find(convertNaN<1,1):find(convertNaN<1,1,'last'));
 timeTrimmed = tdata(find(convertNaN<1,1):find(convertNaN<1,1,'last'));
 %--Interpolate gaps in data
-dataFilled = spline(timeTrimmed,dataTrimmed,tnew);
+dataFilled = spline(timeTrimmed,dataTrimmed,timeTrimmed);
 %--Apply LPF
 [beep,boop] = butter(5,0.3);
 dataFilt = filtfilt(beep,boop,dataFilled);
@@ -15,13 +15,17 @@ dataFilt = filtfilt(beep,boop,dataFilled);
 [~,locs] = findpeaks(-dataFilt);
 %---Preallocate for loop
 nwaves = length(locs)-1;
-tVecNew = zeros(1,length(timeTrimmed));
+tVecNew = NaN(1,length(timeTrimmed));
 for i = 1:nwaves
-    % Make new time vector so all waves go from 0 to 1 (dimensionless)
+    % Convert time vector so all waves go from 0 to 1 (dimensionless)
     tVecNew(locs(i):locs(i+1)) = linspace(0,1,locs(i+1)-locs(i)+1);
 end
+% Trim NaNs out
+
+tVecNew(isnan(tVecNew)) = [];
+dataIn = dataFilt(locs(1):locs(end));
 % Smooth overlaid cycles
-[xOut,yOut] = prepareCurveData(tVecNew,dataFilt);
-flap = fit(xOut, yOut,'smoothingspline');
+[tOut,dataOut] = prepareCurveData(tVecNew,dataIn);
+flap = fit(tOut,dataOut,'smoothingspline');
 end
 
